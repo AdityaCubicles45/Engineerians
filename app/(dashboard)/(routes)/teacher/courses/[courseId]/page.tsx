a0905fe569674b59ea586b05b0a4dda75e3f5ee8
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-02-14 00:22:51 +0530

    course attachment form updated

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,15 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { CircleDashedIcon, CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDashedIcon, CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const CourseIdPage = async ({
     params
@@ -22,10 +24,23 @@ const CourseIdPage = async ({
 
     const course = await db.course.findUnique({
         where: {
-            id: params.courseId
-        }
+            id: params.courseId,
+            userId
+        },
+        include: {
+            chapters: {
+                orderBy: {
+                    position: "asc",
+                },
+            },
+            attachments: {
+                orderBy: {
+                    createdAt: "desc",
+                },
+            },
+        },
     });
-
+ 
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc",
@@ -41,7 +56,8 @@ const CourseIdPage = async ({
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
     ];
 
     const totalFields = requiredFields.length;
@@ -98,9 +114,10 @@ const CourseIdPage = async ({
                                 Course chapters
                             </h2>
                         </div>
-                        <div>
-                            TODO: Chapters
-                        </div>
+                        <ChaptersForm 
+                        initialData={course}
+                        courseId={course.id}
+                    /> 
                     </div>
                     <div>
                         <div className="flex items-center gap-x-2">
@@ -113,6 +130,18 @@ const CourseIdPage = async ({
                             initialData={course}
                             courseId={course.id}
                         />
+                    </div>
+                    <div>
+                    <div className="flex items-center gap-x-2">
+                            <IconBadge icon={File} />
+                            <h2 className="text-xl">
+                                Resources & Attachments
+                            </h2>
+                        </div> 
+                        <AttachmentForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                     </div>
                 </div>
             </div>
